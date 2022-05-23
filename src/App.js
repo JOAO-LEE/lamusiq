3aa7bd08b2ca5c9e9791c6f2ff10d9933cb4511a
João Lee <joaumlimaum@gmail.com>
2022-05-23 19:08:06 -0300

    Requisito 2 concluído

--- src/App.js
+++ src/App.js
@@ -7,19 +7,19 @@ import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import Search from './pages/Search';
 import NotFound from './pages/NotFound';
+import './index.css';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <p>TrybeTunes</p>
         <Switch>
           <Route exact path="/" component={ Login } />
           <Route path="/search" component={ Search } />
           <Route path="/album/:id" component={ Album } />
           <Route path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
           <Route path="/profile/edit" component={ ProfileEdit } />
-          <Route path="/profile" component={ Profile } />
           <Route path="*" component={ NotFound } />
         </Switch>
       </BrowserRouter>);
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,69 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
+// import { Redirect } from 'react-router-dom';
 
 export default class Login extends Component {
+  state = {
+    nameInput: '',
+    accessButton: true,
+    loadingAlert: false,
+    accessGranted: false,
+  }
+
+  handleChangeName = (event) => {
+    const { value } = event.target;
+    this.setState({ nameInput: value }, this.buttonUpdate);
+  }
+
+  buttonUpdate = () => {
+    const { nameInput } = this.state;
+    const requiredLength = 3;
+    if (nameInput.length >= requiredLength) this.setState({ accessButton: false });
+    else {
+      this.setState({ accessButton: true });
+    }
+  }
+
+  loginAuthentication = async () => {
+    const { nameInput } = this.state;
+    this.setState({ loadingAlert: true });
+    await createUser({ name: nameInput });
+    this.setState({ loadingAlert: false, accessGranted: true });
+  }
+
   render() {
+    const { nameInput, accessButton, loadingAlert, accessGranted } = this.state;
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        {loadingAlert ? <Loading />
+          : (
+            <form>
+              <label htmlFor="cadname">
+                Nome:
+                <input
+                  name="loginName"
+                  onChange={ this.handleChangeName }
+                  data-testid="login-name-input"
+                  value={ nameInput }
+                  type="text"
+                  id="cadname"
+                  placeholder="Digite seu nome"
+                />
+                <button
+                  data-testid="login-submit-button"
+                  onClick={ this.loginAuthentication }
+                  name="submitbutton"
+                  disabled={ accessButton }
+                  type="submit"
+                >
+                  Entrar
+                </button>
+              </label>
+            </form>)}
+        { accessGranted && <Redirect to="/search" /> }
+      </div>
     );
   }
 }
