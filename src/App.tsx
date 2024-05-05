2f4e9ff5c76a8f11aad2d4dea67ea24b77aa9d27
João Lee <joaumlimaum@gmail.com>
2024-05-05 17:34:10 -0300

    Albums suggestion section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Heart, Home, Search } from 'lucide-react';
+import { ChevronLeft, ChevronRight, Heart, Home, Search, Play } from 'lucide-react';
 
 
 function App() {
@@ -24,12 +24,53 @@ function App() {
             <a href="" className="text-zinc-400 text-sm hover:text-zinc-100">Gym Soundtrack</a>
             <a href="" className="text-zinc-400 text-sm hover:text-zinc-100">90's R&B</a>
             <a href="" className="text-zinc-400 text-sm hover:text-zinc-100">Best of MPB</a>
-            <a href="" className="text-zinc-400 text-sm hover:text-zinc-100">Club</a>
+            <a href="" className="text-zinc-400 text-sm hover:text-zinc-100">Club Hits</a>
 
           </nav>
         </aside>
         <main className="flex-1 p-6">
-          main
+          <div className="flex items-center gap-6">
+            <button className="rounded-full bg-black/40 p-1">
+              <ChevronLeft />
+            </button>
+            <button className="rounded-full bg-black/40 p-1">
+              <ChevronRight />
+            </button>
+          </div>
+          <h1 className="text-3xl font-semibold mt-10">Good Afternoon</h1>
+          <div className="grid grid-cols-3 gap-4 mt-4">
+            <a href='' className="group rounded bg-white/5 flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
+              <img src="https://source.boomplaymusic.com/group10/M00/5D/77/ced718d43eef461ca50c192e4125cc11_320_320.jpg" alt="Album cover" className="size-20"/>
+              <strong>Album name</strong>
+              <button className="p-2 rounded-full bg-green-400 text-black ml-auto mr-4 invisible group-hover:visible"><Play fill="true"/></button>
+            </a>
+            <a href='' className="group rounded bg-white/5 flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
+              <img src="https://source.boomplaymusic.com/group10/M00/5D/77/ced718d43eef461ca50c192e4125cc11_320_320.jpg" alt="Album cover" className="size-20 "/>
+              <strong>Album name</strong>
+              <button className="p-2 rounded-full bg-green-400 text-black ml-auto mr-4 invisible group-hover:visible transition"><Play fill="true"/></button>
+            </a>
+            <a href='' className="group rounded bg-white/5 flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
+              <img src="https://source.boomplaymusic.com/group10/M00/5D/77/ced718d43eef461ca50c192e4125cc11_320_320.jpg" alt="Album cover" className="size-20"/>
+              <strong>Album name</strong>
+              <button className="p-2 rounded-full bg-green-400 text-black ml-auto mr-4 invisible group-hover:visible transition"><Play fill="true"/></button>
+            </a>
+            <a href='' className="group rounded bg-white/5 flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
+              <img src="https://source.boomplaymusic.com/group10/M00/5D/77/ced718d43eef461ca50c192e4125cc11_320_320.jpg" alt="Album cover" className="size-20"/>
+              <strong>Album name</strong>
+              <button className="p-2 rounded-full bg-green-400 text-black ml-auto mr-4 invisible group-hover:visible transition"><Play fill="true"/></button>
+            </a>
+            <a href='' className="group rounded bg-white/5 flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
+              <img src="https://source.boomplaymusic.com/group10/M00/5D/77/ced718d43eef461ca50c192e4125cc11_320_320.jpg" alt="Album cover" className="size-20"/>
+              <strong>Album name</strong>
+              <button className="p-2 rounded-full bg-green-400 text-black ml-auto mr-4 invisible group-hover:visible transition"><Play fill="true"/></button>
+            </a>
+            <a href='' className="group rounded bg-white/5 flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
+              <img src="https://source.boomplaymusic.com/group10/M00/5D/77/ced718d43eef461ca50c192e4125cc11_320_320.jpg" alt="Album cover" className="size-20"/>
+              <strong>Album name</strong>
+              <button className="p-2 rounded-full bg-green-400 text-black ml-auto mr-4 invisible group-hover:visible transition"><Play fill="true"/></button>
+            </a>
+          </div>
+        
         </main>
       </div>
       <footer className='bg-zinc-800 border-t border-zinc-700 p-6'>
