04fa8a7347432fbdba7fddae7f1d12ae138f544c
João Lee <joaumlimaum@gmail.com>
2024-05-05 15:43:23 -0300

    Sidebar navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import './App.css'
+import { Heart, Home, Search } from 'lucide-react';
+
 
 function App() {
   return (
     <div className="h-screen flex flex-col">
       <div className='flex flex-1'>
         <aside className="w-72 bg-zinc-950 p-6">
-          <nav>
-            <a href="">Home</a>
-            <a href="">Search</a>
-            <a href="">Favorites</a>
+          <div className="flex items-center gap-2">
+            <div className="size-3 rounded-full bg-red-500"/>
+            <div className="size-3 rounded-full bg-yellow-500"/>
+            <div className="size-3 rounded-full bg-green-500"/>
+          </div>
+          <nav className="space-y-5 mt-10">
+            <a href="" className="flex items-center gap-3 text-sm font-semibold text-zinc-200"><Home />Home</a>
+            <a href="" className="flex items-center gap-3 text-sm font-semibold text-zinc-200"><Search />Search</a>
+            <a href="" className="flex items-center gap-3 text-sm font-semibold text-zinc-200"><Heart />Favorites</a>
+          </nav>
+          <nav className="mt-6 pt-10 border-t border-zinc-800 flex flex-col gap-3">
+            <a href="" className="text-zinc-400 text-sm hover:text-zinc-100">Hot Today!</a>
+            <a href="" className="text-zinc-400 text-sm hover:text-zinc-100">Daily Pop</a>
+            <a href="" className="text-zinc-400 text-sm hover:text-zinc-100">French Rap</a>
+            <a href="" className="text-zinc-400 text-sm hover:text-zinc-100">Gym Soundtrack</a>
+            <a href="" className="text-zinc-400 text-sm hover:text-zinc-100">90's R&B</a>
+            <a href="" className="text-zinc-400 text-sm hover:text-zinc-100">Best of MPB</a>
+            <a href="" className="text-zinc-400 text-sm hover:text-zinc-100">Club</a>
 
           </nav>
         </aside>
