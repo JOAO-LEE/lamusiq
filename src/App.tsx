d1b4c3d8ce700ff57393ac37c42613c91e59529c
João Lee <joaumlimaum@gmail.com>
2024-05-05 18:54:31 -0300

    Footer player

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { ChevronLeft, ChevronRight, Heart, Home, Search, Play } from 'lucide-react';
+import { ChevronLeft, ChevronRight, Heart, Home, Search, Play, Shuffle, SkipBack, SkipForward, Repeat, Mic2, LayoutList, SquareMenu, MonitorSpeaker, Volume2, Maximize2 } from 'lucide-react';
 
 
 function App() {
@@ -81,15 +81,58 @@ function App() {
               <span className="text-sm text-zinc-700 group-hover:text-zinc-50 transition-colors duration-500">Josman, Nekfeu, Arma Jackson and more</span> 
             </a>
             <a className="group bg-white/5 p-3 rounded-md flex flex-col gap-2">
-              <img src="https://source.boomplaymusic.com/group10/M00/5D/77/ced718d43eef461ca50c192e4125cc11_320_320.jpg" alt="Album cover" className="group-hover:scale-105 transition duration-500 ease-in-out rounded-lg"/>
+              <img src="https://source.boomplaymusic.com/group10/M00/5D/77/ced718d43eef461ca50c192e4125cc11_320_320.jpg" alt="Album cover" className="group-hover:scale-[1.02] transition duration-500 ease-in-out rounded-lg"/>
               <strong className="group-hover:scale-[1.02] transition duration-500">Daily Mix 1</strong> 
               <span className="text-sm text-zinc-700 group-hover:text-zinc-50 transition-colors duration-500">Josman, Nekfeu, Arma Jackson and more</span> 
             </a>
           </div>
         </main>
       </div>
-      <footer className='bg-zinc-800 border-t border-zinc-700 p-6'>
-        footer
+      <footer className='bg-neutral-900 border-t border-zinc-700 px-6 py-4 flex items-center justify-between'>
+        <div className="flex items-center gap-2">
+          <img src="https://source.boomplaymusic.com/group10/M00/5D/77/ced718d43eef461ca50c192e4125cc11_320_320.jpg" alt="Album cover" className="size-14"/>
+          <div className="flex flex-col">
+            <strong className="font-normal">Elevador</strong>
+            <span className="text-xs text-zinc-500">JoãoLee</span>
+          </div>
+          <Heart className="ml-4 size-5"/>
+        </div>
+        <div className="flex flex-col items-center gap-2">
+          <div className="flex items-center gap-4">
+            <Shuffle size={20}
+            className="text-zinc-200"/>
+            <SkipBack size={20}
+            className="text-zinc-200"/>
+            <button className="p-2 rounded-full bg-zinc-200 text-black"><Play fill="true"/></button>
+            <SkipForward size={20}
+            className="text-zinc-200"/>
+            <Repeat size={20}
+            className="text-zinc-200"/>
+          </div>
+          <div className="group flex justify-between items-center gap-2">
+            <span className="text-zinc-500 text-xs group-hover:text-zinc-300 transition-colors">0:31</span>
+            <div className="h-1 w-96 bg-zinc-600 group-hover:bg-zinc-700 rounded-full">
+              <div className="w-40 bg-zinc-200 h-1 rounded-full"></div>
+            </div>
+            <span className="text-zinc-500 text-xs group-hover:text-zinc-300 transition-colors">4:19</span>
+          </div>
+
+
+        </div>
+      
+        <div className="flex items-center gap-4">
+          <Mic2 size={20} />
+          <LayoutList size={20} />
+          <SquareMenu size={20}/>
+          <MonitorSpeaker size={20} />
+          <div className="flex items-center gap-2">
+            <Volume2 size={20} />
+            <div className="h-1 w-24 bg-zinc-600 group-hover:bg-zinc-700 rounded-full">
+              <div className="w-20 bg-zinc-200 h-1 rounded-full"></div>
+            </div>
+          </div>
+          <Maximize2 size={20}/>
+        </div>
       </footer>
     </div>
   )
