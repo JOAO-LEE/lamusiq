20141c194c23fba7c8004feaefcf1b4e9b9fd5e9
João Lee <joaumlimaum@gmail.com>
2024-05-15 12:19:41 -0300

    Created Search box animation

--- src/shared/components/Sidebar/Search/Search.tsx
+++ src/shared/components/Sidebar/Search/Search.tsx
@@ -4,10 +4,10 @@ import { SearchContext } from "../../../../context/SearchContext/SearchContext"
 import { X } from "lucide-react";
 
 function Search() {
-  const { handleSearch } = useContext(SearchContext);
+  const { handleSearch, isSearchOpen } = useContext(SearchContext);
 
   return (
-    <div className="w-80 absolute bottom-40 left-16 backdrop-blur-sm rounded-md flex flex-col gap-2 mt-5 p-2 border border-zinc-700">
+    <div className={`w-80 absolute backdrop-blur-sm rounded-md flex-col gap-2 mt-5 p-2 border border-zinc-700 ${ isSearchOpen ? "search-box-open" : "search-box-close" }`}>
       <X onClick={() => handleSearch()} className="cursor-pointer hover:scale-110 transition-transform self-end" />
       <SearchForm />
     </div>
--- src/shared/components/Sidebar/Sidebar.tsx
+++ src/shared/components/Sidebar/Sidebar.tsx
@@ -6,14 +6,13 @@ import UserActions from "./UserActions/UserActions";
 import Playlists from "./Playlists/Playlists";
 
 export default function Sidebar() {
-  const { isSearchOpen } = useContext(SearchContext);
 
   return (
-    <div className="w-16 flex-0 flex flex-col items-center p-6 h-min sticky top-0 lg:w-52 lg:items-start" >
+    <div className="w-16 flex-0 flex flex-col items-center p-6 h-min sticky top-0 lg:w-56 lg:items-start" >
       <CosmeticButtons />    
       <UserActions />
       <Playlists />
-      {isSearchOpen ? <Search /> : <></>}
+      <Search />
     </div>
   )
 } 
